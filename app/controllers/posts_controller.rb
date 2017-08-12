6e7cc8196cfb00eef59d91288d7e383bdaa1a7fb
Shisir <netotravels@gmail.com>
2017-08-12 17:37:37 +0000

    Adding log in log out sign up and hiding links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all 
+  end
+  
+  def create
+      @post = Post.find(params[:id])
+      @post.create(post_params)
+      redirect_to root_path
+  end
+  
+      
 end
