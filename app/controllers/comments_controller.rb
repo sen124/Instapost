3994999ccc29aa9691352bd1f12dcd66aa2d5725
Shisir <netotravels@gmail.com>
2017-08-30 15:38:38 +0000

    posting comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: :create
+  
+  def create
+    @post=Post.find(params[:post_id])
+    @comment=@post.comments.create(comment_params.merge[user_id: current_user.id])
+    @comment.valid?
+    redirect_to root_path
+  else
+      flash[:alert]="Invalid Attributes"
+      redirect_to root_path
+  end
+  
+end
+
+
+private
+def comment_params
+    params.require(:comment).permit(:text, :post_id)
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   
   belongs_to :user
-  
+  has_many :comments
   mount_uploader :photo, PhotoUploader
   
   validates  :photo, :description, :user_id, presence: true
